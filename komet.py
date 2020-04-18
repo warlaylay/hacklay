42b56120ab30f25d9e2f1e113169559512c658b6
warlaylay <63898835+warlaylay@users.noreply.github.com>
2020-04-18 14:31:19 +0700

    Add files via upload

---
+++ komet.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import time
+
+os.system('clear')
+time.sleep(1)
+os.system('figlet Hack joker')
+
+print " youtube : sayoga" 
+print " github  : https://github.com/jokersa13"
+print " local   : karangkulon"
+
+time.sleep(1)
+print "[+] menu pilihan [+]"
+print "[1] hack gamepubg"
+print "[2] hack gamemlbb"
+print "[3] hack gameig"
+print "[4] hack gamewa"
+print "[5] install bahan"
+time.sleep(1)
+pilih = raw_input('[?] pilih : ')
+if pilih == "1":
+          os.system('apt update && upgrade')
+          os.syatem('pkg install python2')
+          os.system('pkg install git')
+          os.system('git clone https://github.com/flyngdutchman/Spamcall')
+elif pilih == "2":
+          os.system('git clone https://github.com/flyngdutchman/SpamCall')
+          time.sleep(1)
+          os.system('apt update && upgrade')
+          os.syatem('pkg install python2')
+          os.system('pkg install git')
+          os.system('git clone https://github.com/flyngdutchman/SpamCall')
+          time.sleep(1)
+elif pilih == "3":
+          os.system('apt update && upgrade')
+          os.syatem('pkg install python2')
+          os.system('pkg install git')
+          os.system('git clone https://github.com/flyngdutchman/SpamCall')
+          time.sleep(1)
+elif pilih == "4":
+          os.system('apt update && upgrade')
+          os.syatem('pkg install python2')
+          os.system('pkg install git')
+          os.system('git clone https://github.com/flyngdutchman/SpamCall')
+          time.sleep(1)
+elif pilih == "5":
+          os.system('apt update && upgrade')
+          time.sleep(1)
+          os.system('pkg install python2')
+          time.sleep(1)
+          os.system('pkg install nano')
+          time.sleep(1)
+          os.system('pkg install git')
+          time.sleep(1)
+          os.system('pip2 install requests mechanize')
+          time.sleep(1)
+          print "[+] penginstalan selesai"
